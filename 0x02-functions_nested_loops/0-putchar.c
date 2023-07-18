676500302124447fa22588815682d4df796b59b9
Assil <haveyoustarted@gmail.com>
2023-07-18 11:01:22 -0700

    X

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,28 +1,21 @@
-#include <stdio.h> // Include the necessary header file
-
-int _putchar(char c); // Declare the _putchar function
-
+#include "main.h"
 /**
- * main - Entry point of the program
- * Return: 0 on successful execution
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return 0;
-}
-
-int _putchar(char c)
-{
-    return putchar(c); // Implement the _putchar function (this uses the putchar function from stdio.h)
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
